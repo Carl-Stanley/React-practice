1442ae1310ae1637dabdb9e4cda9b3966464742d
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-09-01 12:41:03 -0700

    commit

---
+++ practice/src/components/MyComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyApp  () {
+
+    return (
+
+          <ul>
+              <li>This</li>
+              <li>Is</li>
+              <li>A</li>
+              <li>Test</li>
+          </ul>  
+
+    );
+}
+
+export default MyApp;

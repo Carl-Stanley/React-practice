887b99523bde3957bc361cfc076b17effee0d5b3
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-10-17 08:50:17 -0700

    commit

--- practice/src/components/MessageComponent.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Message = props => <h4>{props.propsMessage} </h4>
-
-export default Message; 
--- practice/src/components/NameComponent.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Name = props => <h4>{props.propsName}</h4>
-
-export default Name; 

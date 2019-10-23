b3601176554d9401d5563a5a0cf69e82240f1906
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-10-23 10:11:14 -0700

    Commit

--- build/src/App.js
+++ build/src/App.js
@@ -1,47 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import './App.css'; 
+import {axiosWithAuth} from "./axiosWithAuth";
 
-
-var thetoken = '';
+ 
 
 
 
 function App() {
-     
-  axios({
-    method: 'post',
-    url: 'https://corporate-event-planner-webeu.herokuapp.com/api/auth/login',
-    data: {
-      "email": "collette@test.com",
-      "password": "123456"
-    }
-  })
-  .then(function (response) {
-    
-    thetoken = response.data.token;
-    console.log(thetoken);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+   
+  
+  axiosWithAuth()
+			
+			.get("/api/events")
+			.then(res => {
+				console.log(res);
+				
+			})
+			.catch(error => {
+				console.log(error);
+			});
+  //axios({
+  //  method: 'post',
+   // url: '/api/auth/login',
+   // data: {
+    //  "email": "collette@test.com",
+     // "password": "123456"
+   // }
+  //})
+  //.then(function (response) {
+   // 
+   // thetoken = response.data.token;
+   // console.log(thetoken);
+  //})
+  //.catch(function (error) {
+    //console.log(error);
+  //});
  
 
-  axios({
-    method: 'get',
-    url: 'https://corporate-event-planner-webeu.herokuapp.com/api/events',
-    headers: {
-      Authorization: toString(`${thetoken}`)       
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-    })
-  .catch(function (error) {
-    console.log(error);
-    console.log(`${thetoken}`);
-  });
+  //axios({
+   // method: 'get',
+   // url: 'https://corporate-event-planner-webeu.herokuapp.com/api/events',
+    //headers: {
+     // 'Authorization': token,
+    //  'Content-Type': 'application/json'
+   //},
+  //})
+  //.then(function (response) {
+   // console.log(response);
+    //})
+  //.catch(function (error) {
+   // console.log(error);
+    //console.log(`${thetoken}`);
+  //});
  
   return (
     <div className="App">
